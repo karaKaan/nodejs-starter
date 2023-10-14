2a3e7db827330bd80217705e053bea0cac34eb4e
KaanKara <41960384+karaKaan@users.noreply.github.com>
2023-10-14 15:42:09 +0200

    feat: init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 require("dotenv").config();
-
-const sumNumbers = (numbers) => {
-  return numbers.reduce((prev, curr) => prev + curr);
-};
-
-const sumNumber = sumNumbers([1, 2, 3]);
-console.log(sumNumber);
-
-module.exports = { sumNumbers };
